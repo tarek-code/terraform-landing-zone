613b9ccc8480abedc3f6c5cae41fe5f6b9d73750
tarek-code <tarekadel314@gmail.com>
2025-06-27 21:05:22 +0300

    maybe

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "poc-prod-tfstate-2025"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "poc-prod-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
