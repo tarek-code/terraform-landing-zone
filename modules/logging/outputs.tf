13dd75b31969ea31bf1740cea5ae98af626cecba
tarek-code <tarekadel314@gmail.com>
2025-06-27 22:56:35 +0300

    fix(logging)

--- modules/logging/outputs.tf
+++ modules/logging/outputs.tf
@@ -1,7 +1,9 @@
 output "cloudwatch_log_group_name" {
-  value = aws_cloudwatch_log_group.vpc_log_group.name
+  description = "The name of the CloudWatch log group"
+  value       = aws_cloudwatch_log_group.vpc_flow_logs.name
 }
 
 output "s3_log_bucket" {
-  value = aws_s3_bucket.flow_logs_bucket.bucket
+  description = "The name of the S3 bucket for VPC Flow Logs"
+  value       = aws_s3_bucket.log_bucket.bucket
 }
